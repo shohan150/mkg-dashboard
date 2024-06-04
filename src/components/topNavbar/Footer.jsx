55491131d4d62a2480ad56fdb3f196262482f467
shohan <shohan.ece17@gmail.com>
2024-06-04 13:01:37 +0600

    reponsive

--- src/components/topNavbar/Footer.jsx
+++ src/components/topNavbar/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <div className="p-4 sm:flex items-center justify-between bg-primary text-[#ffffff85] text-sm rounded-md mt-4 leading-6">
-      <h4>@2024 all rights are reserved by continental ventures limited</h4>
-      <h4>Privacy policy | Terms of use</h4>
+      <h4 className="text-center">
+        @2024 all rights are reserved by continental ventures limited
+      </h4>
+      <h4 className="text-center">Privacy policy | Terms of use</h4>
     </div>
   );
 }
