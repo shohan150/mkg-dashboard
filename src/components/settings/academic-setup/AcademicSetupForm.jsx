9276d571f4e74bc7f6786afdd881899ed2a46621
shohan <shohan.ece17@gmail.com>
2024-06-08 17:21:52 +0600

    tried floating label. but radio, check and select will always have the label floating

--- src/components/settings/academic-setup/AcademicSetupForm.jsx
+++ src/components/settings/academic-setup/AcademicSetupForm.jsx
@@ -3,7 +3,8 @@ export default function AcademicSetupForm() {
     <div className="bg-white rounded-md px-4 py-2 my-2 sm:my-4">
       <form className="md:flex gap-7 items-center justify-between">
         {/* select what type of data to entry */}
-        <div className="flex items-center gap-2 md:w-5/12 my-2">
+        {/* previous select field */}
+        {/* <div className="flex items-center gap-2 md:w-5/12 my-2">
           <label className="w-28 md:max-lg:w-32">Select Type</label>
           <select
             id=""
@@ -18,15 +19,58 @@ export default function AcademicSetupForm() {
             <option value="Period">Period</option>
             <option value="Session">Session</option>
           </select>
+        </div> */}
+
+        {/* new select field */}
+        <div className="relative md:w-5/12 my-2">
+          <select
+            name="type name"
+            className="peer bg-bgGray w-full rounded p-2 border-2 border-transparent focus:border-primary focus:outline-none placeholder-transparent"
+          >
+            <option value="Class" disabled>
+              Class
+            </option>
+            <option value="Subject">Subject</option>
+            <option value="Section">Section</option>
+            <option value="Shift">Shift</option>
+            <option value="Period">Period</option>
+            <option value="Session">Session</option>
+          </select>
+          <label
+            htmlFor="type name"
+            className="absolute left-2 -top-3 text-sm transition-all duration-200 bg-white px-1 peer-focus:-top-3 peer-focus:bg-white peer-focus:text-sm
+            peer-placeholder-shown:text-base peer-placeholder-shown:top-[10px] peer-placeholder-shown:bg-transparent"
+          >
+            Type Name
+          </label>
         </div>
+
         {/* enter the value */}
-        <div className="flex items-center gap-2 md:w-5/12 my-2">
+        {/* previous form with static label and input field.  */}
+        {/* <div className="flex items-center gap-2 md:w-5/12 my-2">
           <label className="w-28 md:max-lg:w-32">Type Name</label>
           <input
             type="text"
             placeholder="Enter Item Name"
             className="bg-bgGray w-full rounded p-2 border-2 border-transparent focus:border-primary focus:outline-none"
           />
+        </div> */}
+
+        {/* New input field with floating label*/}
+        <div className="relative md:w-5/12 my-4">
+          <input
+            type="text"
+            name="type name"
+            className="peer bg-bgGray w-full rounded p-2 border-2 border-transparent focus:border-primary focus:outline-none placeholder-transparent"
+            placeholder="Type Name"
+          />
+          <label
+            htmlFor="type name"
+            className="absolute left-2 -top-3 text-sm transition-all duration-200 bg-white px-1 peer-focus:-top-3 peer-focus:bg-white peer-focus:text-sm
+            peer-placeholder-shown:text-base peer-placeholder-shown:top-[10px] peer-placeholder-shown:bg-transparent"
+          >
+            Type Name
+          </label>
         </div>
 
         {/* create button */}
