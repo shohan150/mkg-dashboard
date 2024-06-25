8dc3f404306a4f04d9df52c8e2669608b9372e53
shohan <shohan.ece17@gmail.com>
2024-06-25 18:51:23 +0600

    working on chooseable subject table

--- src/components/settings/ChooseableSubject.jsx
+++ src/components/settings/ChooseableSubject.jsx
@@ -1,4 +1,4 @@
-import academicSetupList from "../../data/academicSetupList";
+// import ChooseSubTable from "./chooseable-subject/ChooseSubTable";
 import SelectClass from "./chooseable-subject/SelectClass";
 import SelectSubject from "./chooseable-subject/SelectSubject";
 
@@ -8,22 +8,28 @@ export default function ChooseableSubject() {
       {/* select a particular class */}
       <SelectClass />
 
-      {/* show all subjects */}
-      <div className="grid grid-cols-3 sm:grid-cols-5 my-6">
-        {academicSetupList[2].content.map((singleSubject, index) => (
-          <div key={index} className="space-x-2 border-b-2 py-3">
+      {/* show all students */}
+      {/* <ChooseSubTable/> */}
+      <div className="h-40">
+      <table className="w-full text-textGray text-sm leading-8 my-6">
+      <thead className="border-b-2">
+        <tr>
+          <th className="w-1/12">
             <input
               type="checkbox"
-              id=""
-              name=""
-              value="hi"
-              className="text-primary"
-            ></input>
-            <label htmlFor="vehicle1">{singleSubject.info}</label>
-          </div>
-        ))}
+            />
+          </th>
+          <th className="w-2/12">STUDENT ID</th>
+          <th className="w-3/12">NAME</th>
+          <th className="w-2/12">ROLL</th>
+          <th className="w-2/12">CLASS</th>
+          <th className="w-2/12">SECTION</th>
+        </tr>
+      </thead>
+      </table>
       </div>
 
+      
       <SelectSubject />
 
       {/* save button */}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
   const sidebarRef = useRef(null);
 
   useEffect(() => {
-    // check if clicked outside of sidebar area. if dom node exists, means sidebar is open, then sidebarRef.current has value. if so, then, check if it contains the clicked element on every mousedown event.
+    // check if clicked outside of sidebar.
     const handleClickOutside = (event) => {
       if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
         setShowSidebar(false);
@@ -25,7 +25,7 @@ export default function Sidebar({ showSidebar, setShowSidebar }) {
       <nav
       ref={sidebarRef}
         id="sidebar-menu"
-        className={`fixed transition-all duration-500 ease-in-out h-screen bg-primary bg-[linear-gradient(270deg,rgba(51,148,225,0.18),transparent)] shadow-sm w-0 xl:w-72 scrollbar ${
+        className={`fixed transition-all duration-200 ease-in-out h-screen bg-primary bg-[linear-gradient(270deg,rgba(51,148,225,0.18),transparent)] shadow-sm w-0 xl:w-72 scrollbar ${
           showSidebar && "w-72"
         } z-20`}
       >
