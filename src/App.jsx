a480c20b8d817071fb058ecc7464665ec94070e2
shohan <shohan.ece17@gmail.com>
2024-06-25 16:25:27 +0600

    small device menu close on clicking outside

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,23 @@ export default function App() {
   const { pathname } = useLocation();
   useEffect(() => {
       window.scrollTo(0, 0);
+      //also close sidebar on mobile on route change.
+      setShowSidebar(false);
   }, [pathname]);
 
   return (
     // sidebar and topNavbar common in all pages
-    <div className="font-roboto text-base font-normal text-gray-600 dark:text-gray-400 dark:bg-gray-800">
-        <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
-        <div className="bg-secondary text-textBlack flex flex-col justify-between min-h-screen transition-all duration-500 ease-in-out p-3 ml-0 xl:ml-72">
-          <div>
-            <TopNavbar setShowSidebar={setShowSidebar} />
-            <Outlet />
-          </div>
-          <Footer />
+    <div className="font-roboto text-base font-normal text-gray-600 dark:text-gray-400 dark:bg-gray-800 relative">
+      <Sidebar showSidebar={showSidebar} setShowSidebar={setShowSidebar} />
+      <div className={`bg-secondary text-textBlack flex flex-col justify-between min-h-screen transition-all duration-500 ease-in-out p-3 ml-0 xl:ml-72 ${showSidebar && "max-xl:opacity-65"}`}>
+        <div>
+          <TopNavbar setShowSidebar={setShowSidebar} />
+          <Outlet />
         </div>
-        {/* an empty div to contain modals and popups */}
-        <div id="portal-root"></div>
+        <Footer />
+      </div>
+      {/* an empty div to contain modals and popups */}
+      <div id="portal-root"></div>
     </div>
   );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,23 +1,42 @@
+import { useEffect, useRef } from "react";
 import { MdHighlightOff } from "react-icons/md";
 import SidebarHeader from "./SidebarHeader";
 import SidebarMenu from "./SidebarMenu";
 
 export default function Sidebar({ showSidebar, setShowSidebar }) {
+
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    // check if clicked outside of sidebar area. if dom node exists, means sidebar is open, then sidebarRef.current has value. if so, then, check if it contains the clicked element on every mousedown event.
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        setShowSidebar(false);
+      }
+    };
+    //function assigned to eventListener.
+    document.addEventListener('mousedown', handleClickOutside);
+
+    //callback to remove the eventLitener.
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [setShowSidebar]);
+
   return (
-    <nav
-      id="sidebar-menu"
-      className={`fixed transition-all duration-500 ease-in-out h-screen bg-primary bg-[linear-gradient(270deg,rgba(51,148,225,0.18),transparent)] shadow-sm w-0 xl:w-72 scrollbar ${
-        showSidebar && "w-72"
-      } z-50`}
-    >
-      <div className="h-full overflow-y-auto relative scrollbar-webkit">
-        <SidebarHeader />
-        <SidebarMenu />
-        <MdHighlightOff
-          className="text-white w-6 h-6 absolute top-[18px] right-3 xl:hidden"
-          onClick={() => setShowSidebar(false)}
-        />
-      </div>
-    </nav>
+      <nav
+      ref={sidebarRef}
+        id="sidebar-menu"
+        className={`fixed transition-all duration-500 ease-in-out h-screen bg-primary bg-[linear-gradient(270deg,rgba(51,148,225,0.18),transparent)] shadow-sm w-0 xl:w-72 scrollbar ${
+          showSidebar && "w-72"
+        } z-20`}
+      >
+        <div className="h-full overflow-y-auto relative scrollbar-webkit">
+          <SidebarHeader />
+          <SidebarMenu />
+          <MdHighlightOff
+            className="text-white w-6 h-6 absolute top-[18px] right-3 xl:hidden"
+            onClick={() => setShowSidebar(false)}
+          />
+        </div>
+      </nav>
   );
 }
