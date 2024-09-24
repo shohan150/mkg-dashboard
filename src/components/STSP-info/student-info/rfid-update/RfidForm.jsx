272e5231053a92a4ed9b69813ae16869b0297888
shohan <shohan.ece17@gmail.com>
2024-09-24 16:48:16 +0600

    rfid form small update

--- src/components/STSP-info/student-info/rfid-update/RfidForm.jsx
+++ src/components/STSP-info/student-info/rfid-update/RfidForm.jsx
@@ -9,6 +9,7 @@ export default function RfidForm() {
             defaultValue="Class"
             className="bg-bgGray w-full rounded px-1 py-2 border-2 border-transparent focus:border-primary focus:outline-none"
          >
+            <option value="Class">Class</option>
             <option value="Nursery">Nursery</option>
             <option value="One">One</option>
             <option value="Two">Two</option>
@@ -26,6 +27,7 @@ export default function RfidForm() {
             name=""
             className="bg-bgGray w-full rounded px-1 py-2 border-2 border-transparent focus:border-primary focus:outline-none"
          >
+            <option value="Section">Section</option>
             <option value="Jaba">Jaba</option>
             <option value="Golap">Golap</option>
          </select>
