7427cad500e3d0a4ad6881f0d3f4b615dc9e7feb
shohan <shohan.ece17@gmail.com>
2024-11-14 17:49:34 +0600

    title update

--- src/components/fees/QuickCollection.jsx
+++ src/components/fees/QuickCollection.jsx
@@ -10,7 +10,7 @@ const QuickCollection = () => {
 
   return (
     <div className="bg-white rounded-md p-4 md:p-6  my-4">
-      <h3 className="text-2xl font-medium">{t("module.fees.class_wise_collection")}</h3>
+      <h3 className="text-2xl font-medium">{t("module.fees.quick_collection")}</h3>
       <QuickColForm />
       <StudentShowcase />
       <ClsWiseTable />
