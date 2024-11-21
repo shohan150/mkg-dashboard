43981e8619e81848073f512173c083b0862037b0
shohan <shohan.ece17@gmail.com>
2024-11-21 15:37:20 +0600

    ins profile data shown

--- src/components/instituteProfile/InstituteContact.jsx
+++ src/components/instituteProfile/InstituteContact.jsx
@@ -3,8 +3,10 @@ import { FaRegEnvelope, FaRegStar } from "react-icons/fa6";
 import { IoPerson } from "react-icons/io5";
 import { MdOutlineLocalPhone } from "react-icons/md";
 
-export default function InstituteContact() {
+export default function InstituteContact({data}) {
    const {t} = useTranslation();
+
+   const {headmaster_mobile, headmaster_name, ict_teacher, ict_teacher_email, ict_teacher_mobile, institute_email_address} = data;
 
     return (
       <div className="md:w-1/2 p-6 bg-white rounded-md text-textGray mb-4 md:mb-0">
@@ -12,33 +14,33 @@ export default function InstituteContact() {
          <div className="flex items-center gap-2 leading-9">
             <IoPerson />
             <h5 className="font-medium">{t("module.instituteInfo.principal")}:</h5>
-            <h5>{t("module.instituteInfo.principalName")}</h5>
+            <h5>{headmaster_name}</h5>
          </div>
          <div className="flex items-center gap-2 leading-9">
             <MdOutlineLocalPhone />
             <h5 className="font-medium">{t("module.instituteInfo.principal")} {t("general.mobile")}:</h5>
-            <h5>{t("module.instituteInfo.principalMobile")}</h5>
+            <h5>{headmaster_mobile}</h5>
          </div>
          <div className="flex items-center gap-2 leading-9">
             <FaRegStar />
             <h5 className="font-medium">
                {t("module.instituteInfo.incharge")}:</h5>
-            <h5>{t("module.instituteInfo.inchargeName")}</h5>
+            <h5>{ict_teacher}</h5>
          </div>
          <div className="flex items-center gap-2 leading-9">
             <MdOutlineLocalPhone />
             <h5 className="font-medium">{t("module.instituteInfo.incharge")} {t("general.mobile")}:</h5>
-            <h5>{t("module.instituteInfo.inchargeMobile")}</h5>
+            <h5>{ict_teacher_mobile}</h5>
          </div>
          <div className="flex items-center gap-2 leading-9">
             <FaRegEnvelope />
             <h5 className="font-medium">{t("module.instituteInfo.incharge")} {t("general.email")}:</h5>
-            <h5>incharge22@gmail.com</h5>
+            <h5>{ict_teacher_email}</h5>
          </div>
          <div className="flex items-center gap-2 leading-9">
             <FaRegEnvelope />
             <h5 className="font-medium">{t("module.instituteInfo.incharge")} {t("general.email")}:</h5>
-            <h5>urbanschool@gmail.com</h5>
+            <h5>{institute_email_address}</h5>
          </div>
       </div>
     );
--- src/components/instituteProfile/InstituteInfo.jsx
+++ src/components/instituteProfile/InstituteInfo.jsx
@@ -2,8 +2,10 @@ import { useTranslation } from "react-i18next";
 import { GoDotFill } from "react-icons/go";
 
 
-export default function InstituteInfo() {
+export default function InstituteInfo({data}) {
    const {t} = useTranslation();
+
+   const {institute_id, institute_name, institute_eiin_no, education_board_id, education_division_id, education_district_id, education_thana_id, status } = data;
 
     return (
       <div className="md:w-1/2 p-6 bg-white rounded-md text-textGray">
@@ -11,42 +13,42 @@ export default function InstituteInfo() {
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("general.id")}:</h5>
-            <h5>{t("module.instituteInfo.idNumber")}</h5>
+            <h5>{institute_id}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("module.instituteInfo.eiin")}:</h5>
-            <h5>{t("module.instituteInfo.eiinNumber")}</h5>
+            <h5>{institute_eiin_no}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("general.name")}:</h5>
-            <h5>{t("module.instituteInfo.schoolName")}</h5>
+            <h5>{institute_name}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.education")} {t("module.instituteInfo.board")}:</h5>
-            <h5>{t("module.instituteInfo.boardName")}</h5>
+            <h5>{}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("module.instituteInfo.division")}:</h5>
-            <h5>{t("module.instituteInfo.divisionName")}</h5>
+            <h5>{}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("module.instituteInfo.district")}:</h5>
-            <h5>{t("module.instituteInfo.districtName")}</h5>
+            <h5>{}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("module.instituteInfo.thana")}:</h5>
-            <h5>{t("module.instituteInfo.thanaName")}</h5>
+            <h5>{}</h5>
          </div>
          <div className="flex items-center gap-2 leading-7">
             <GoDotFill className="text-xs"/>
             <h5 className="font-medium">{t("general.institute")} {t("general.status")}:</h5>
-            <h5>{t("module.instituteInfo.active")}</h5>
+            <h5>{status}</h5>
          </div>
       </div>
     );
