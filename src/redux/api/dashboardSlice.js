46f47da61e3f7a8e714de29ff78e9203b7eb4017
shohan <shohan.ece17@gmail.com>
2024-11-21 20:28:47 +0600

    status code

--- src/redux/api/dashboardSlice.js
+++ src/redux/api/dashboardSlice.js
@@ -3,7 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const dashboardSlice = createApi({ 
    reducerPath: "dashboard",
    baseQuery: fetchBaseQuery({
-      baseUrl: `http://madani.easydr.xyz/api`,
+      baseUrl: `${import.meta.env.VITE_SERVER_BASE_URL}/api`,
+      prepareHeaders: (headers) => {
+         headers.set("Content-Type", "application/json");
+         return headers;
+       },
    }),
 
     //define all the api endpoints. Follows the builder pattern. 
