6db99ef23622c07beb77edb5af4a77cc9ac1b544
shohan <shohan.ece17@gmail.com>
2024-11-21 20:14:42 +0600

    status code

--- src/components/common/Error.jsx
+++ src/components/common/Error.jsx
@@ -6,7 +6,7 @@ const Error = ({ errorMessage, code, imageNo = 1 }) => {
     <div className="flex flex-col items-center justify-center min-h-screen p-12">
       <img src={image} alt="Error" className="w-48 h-48" />
       <p className="text-xl font-bold text-red-500 mt-4">An Error Has Occurred</p>
-      <p className="text-white mt-2">Status code: {code}. {errorMessage}</p>
+      <p className="text-white mt-2">{code && `Status code: ${code}.`} {errorMessage}</p>
     </div>
   );
 };
